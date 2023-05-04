a5f3c81d311793ec7d401efee5269640f465bb26
lvaibhav <111556747+lvaibhav@users.noreply.github.com>
2023-05-04 20:27:40 +0530

    Update server_stop_clean.sh

--- scripts/server_stop_clean.sh
+++ scripts/server_stop_clean.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Stop Tomcat first
-sudo systemctl stop tomcat
-sudo rm -rf /usr/share/tomcat/webapps/hello-world-maven.war
-
+#sudo systemctl stop tomcat
+#sudo rm -rf /usr/share/tomcat/webapps/hello-world-maven.war
+#midified as per current reuiremnt but please refere above only
+cd /opt/tomcat/apache-tomcat-9.0.74/bin
+./catalina.sh stop
+sudo rm -rf /opt/tomcat/apache-tomcat-9.0.74/webapps/*.war
